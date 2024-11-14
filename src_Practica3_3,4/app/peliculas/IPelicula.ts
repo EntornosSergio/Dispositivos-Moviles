daed4eb3c1355c91e062919e22c826e9efd14e85
SergisDAW <165885638+EntornosSergio@users.noreply.github.com>
2024-11-14 10:29:23 +0100

    Delete src_Practica3_3,4 directory

--- src_Practica3_3,4/app/peliculas/IPelicula.ts
+++
@@ -1,9 +0,0 @@
-export interface IPeliculas {
-    caratula: string;
-    titulo: string;
-    year: number;
-    director: string;
-    sinopsis: string;
-    puntuacion: number;
-    image: string;
-}
--- src_Practica3_3,4/app/series/ISerie.ts
+++
@@ -1,7 +0,0 @@
-export interface ISerie {
-    caratula:string;
-    titulo:string;
-    temporadas:number;
-    sinopsis:string;
-    plataforma:string;
-}
